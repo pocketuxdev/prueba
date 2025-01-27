3aeb07ec850f25aa57b4df86d005e5af79fa40ea
pocketuxdev <pocketuxdev@gmail.com>
2025-01-27 15:18:48 -0500

    titulo dasg

--- main.py
+++ main.py
@@ -1728,7 +1728,8 @@ def dashboard_page():
                             <i class="fas fa-chart-line header-icon"></i>
                             <span class="title-container">
                                 <span class="title-main">Tiffany</span>
-                                <span class="title-sub">Medical Assistant</span>
+                                <span class="title-sub">Medical</span>
+                                <span class="title-sub">Assistant</span>
                             </span>
                         </h1>
                     </div>
