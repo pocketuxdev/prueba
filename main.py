6301ea73fdae79c9dc4cf43b88e5464e41e9e944
pocketuxdev <pocketuxdev@gmail.com>
2025-02-07 16:46:29 -0500

    titulo

--- main.py
+++ main.py
@@ -4537,7 +4537,7 @@ def vitafer_dashboard():
     return f"""
     <html>
         <head>
-            <title>Tiffany Medical Assistant - Dashboard</title>
+            <title>Tiffany Vitafer - Dashboard</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
